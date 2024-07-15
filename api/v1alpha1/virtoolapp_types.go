24b284c3e83baa773cebb8687bd62d0a53f4c8fa
Brycycle <44349959+Bryce-Davidson@users.noreply.github.com>
2024-07-15 11:43:17 -0700

    feat: simplify CRD

--- api/v1alpha1/virtoolapp_types.go
+++ api/v1alpha1/virtoolapp_types.go
@@ -19,104 +19,67 @@ package v1alpha1
 import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
-// VirtoolAppSpec defines the desired state of VirtoolApp
+// VirtoolAppSpec defines the desired state of the application
 type VirtoolAppSpec struct {
-	// Components is a map of component names to their desired specifications
-	Components map[string]ComponentSpec `json:"components"`
+	// Version is the desired version of the application
+	Version string `json:"version"`
 
-	// UpdateStrategy defines how updates should be performed
-	UpdateStrategy UpdateStrategy `json:"updateStrategy,omitempty"`
-
-	// GlobalConfig defines global settings for all components
-	GlobalConfig GlobalConfig `json:"globalConfig,omitempty"`
+	// Components is a list of components for the application
+	Components []ComponentSpec `json:"components"`
 }
 
 // ComponentSpec defines the specification for a single component
 type ComponentSpec struct {
-	// Version is the desired version of the component
-	Version string `json:"version"`
+	// Name is the name of the component
+	Name string `json:"name"`
 
 	// Image is the container image for the component
 	Image string `json:"image"`
 
 	// Replicas is the desired number of replicas for the component
-	Replicas *int32 `json:"replicas,omitempty"`
+	Replicas int32 `json:"replicas,omitempty"`
 
 	// Resources defines the resource requirements for the component
 	Resources corev1.ResourceRequirements `json:"resources,omitempty"`
 
-	// UpdateOrder defines the order in which this component should be updated
-	UpdateOrder int `json:"updateOrder,omitempty"`
-
 	// PreUpdateJob defines a job to run before updating this component
-	PreUpdateJob *Job `json:"preUpdateJob,omitempty"`
+	PreUpdateJob *JobSpec `json:"preUpdateJob,omitempty"`
 
 	// PostUpdateJob defines a job to run after updating this component
-	PostUpdateJob *Job `json:"postUpdateJob,omitempty"`
+	PostUpdateJob *JobSpec `json:"postUpdateJob,omitempty"`
 }
 
-// Job defines a job to be run as part of the update process
-type Job struct {
+// JobSpec defines a job to be run as part of the update process
+type JobSpec struct {
 	Image   string   `json:"image"`
 	Command []string `json:"command,omitempty"`
 	Args    []string `json:"args,omitempty"`
 }
 
-// UpdateStrategy defines how updates should be performed
-type UpdateStrategy struct {
-	// Type is the type of update strategy (e.g., "RollingUpdate", "Recreate")
-	Type string `json:"type"`
-
-	// MaxUnavailable defines the maximum number of pods that can be unavailable during the update
-	MaxUnavailable *intstr.IntOrString `json:"maxUnavailable,omitempty"`
-
-	// MaxSurge defines the maximum number of pods that can be created over the desired number of pods
-	MaxSurge *intstr.IntOrString `json:"maxSurge,omitempty"`
-}
-
-// GlobalConfig defines global settings for all components
-type GlobalConfig struct {
-	// Registry is the default container registry to use
-	Registry string `json:"registry,omitempty"`
-
-	// ImagePullSecret is the name of the secret containing registry credentials
-	ImagePullSecret string `json:"imagePullSecret,omitempty"`
-
-	// Tolerations defines the tolerations for all components
-	Tolerations []corev1.Toleration `json:"tolerations,omitempty"`
-
-	// NodeSelector defines the node selector for all components
-	NodeSelector map[string]string `json:"nodeSelector,omitempty"`
-}
-
-// VirtoolAppStatus defines the observed state of VirtoolApp
+// VirtoolAppStatus defines the observed state of the application
 type VirtoolAppStatus struct {
-	// ComponentStatus tracks the status of individual components
-	ComponentStatus map[string]ComponentStatus `json:"componentStatus"`
+	// CurrentVersion is the current version of the application
+	CurrentVersion string `json:"currentVersion"`
+
+	// ComponentsStatus tracks the status of individual components
+	ComponentsStatus []ComponentStatus `json:"componentsStatus"`
 
 	// Conditions represent the latest available observations of the VirtoolApp's state
-	Conditions []metav1.Condition `json:"conditions"`
-
-	// LastUpdateTime is the last time the status was updated
-	LastUpdateTime metav1.Time `json:"lastUpdateTime"`
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
 
 // ComponentStatus tracks the status of an individual component
 type ComponentStatus struct {
+	// Name is the name of the component
+	Name string `json:"name"`
+
 	// CurrentVersion is the current version of the component
 	CurrentVersion string `json:"currentVersion"`
 
-	// DesiredVersion is the version the component is being updated to
-	DesiredVersion string `json:"desiredVersion"`
-
 	// Status is the current status of the component
 	Status string `json:"status"`
-
-	// Message provides additional information about the component's status
-	Message string `json:"message,omitempty"`
 
 	// ReadyReplicas is the number of replicas that are ready
 	ReadyReplicas int32 `json:"readyReplicas"`
@@ -130,10 +93,11 @@ type ComponentStatus struct {
 
 // VirtoolApp is the Schema for the virtoolapps API
 type VirtoolApp struct {
-	Spec              VirtoolAppSpec   `json:"spec,omitempty"`
-	Status            VirtoolAppStatus `json:"status,omitempty"`
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   VirtoolAppSpec   `json:"spec,omitempty"`
+	Status VirtoolAppStatus `json:"status,omitempty"`
 }
 
 //+kubebuilder:object:root=true
--- factory/virtoolapp.go
+++ factory/virtoolapp.go
@@ -4,7 +4,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/intstr"
 
 	virtoolv1alpha1 "github.com/bryce-davidson/virtool-operator/api/v1alpha1"
 )
@@ -17,18 +16,13 @@ func pointer(i int32) *int32 {
 type VirtoolAppOption func(*virtoolv1alpha1.VirtoolApp)
 
 const (
-	defaultVersion         = "1.0.0"
-	defaultImage           = "default-image:latest"
-	defaultReplicas        = 1
-	defaultCPULimit        = "100m"
-	defaultMemoryLimit     = "128Mi"
-	defaultCPURequest      = "50m"
-	defaultMemoryRequest   = "64Mi"
-	defaultUpdateType      = "RollingUpdate"
-	defaultMaxUnavailable  = "25%"
-	defaultMaxSurge        = "25%"
-	defaultRegistry        = "default-registry.example.com"
-	defaultImagePullSecret = "default-pull-secret"
+	defaultVersion       = "1.0.0"
+	defaultImage         = "default-image:latest"
+	defaultReplicas      = 1
+	defaultCPULimit      = "100m"
+	defaultMemoryLimit   = "128Mi"
+	defaultCPURequest    = "50m"
+	defaultMemoryRequest = "64Mi"
 )
 
 func defaultResourceRequirements() corev1.ResourceRequirements {
@@ -48,35 +42,26 @@ func NewVirtoolApp(name, namespace string, opts ...VirtoolAppOption) *virtoolv1a
 	v := &virtoolv1alpha1.VirtoolApp{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "cache.example.com/v1alpha1",
-			Kind:       "virtoolv1alpha1.VirtoolApp",
+			Kind:       "VirtoolApp",
 		},
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
 			Namespace: namespace,
 		},
 		Spec: virtoolv1alpha1.VirtoolAppSpec{
-			Components: map[string]virtoolv1alpha1.ComponentSpec{
-				"default": {
-					Version:     defaultVersion,
-					Image:       defaultImage,
-					Replicas:    pointer(int32(defaultReplicas)),
-					UpdateOrder: 0,
-					Resources:   defaultResourceRequirements(),
+			Version: defaultVersion,
+			Components: []virtoolv1alpha1.ComponentSpec{
+				{
+					Name:      "default",
+					Image:     defaultImage,
+					Replicas:  defaultReplicas,
+					Resources: defaultResourceRequirements(),
 				},
-			},
-			UpdateStrategy: virtoolv1alpha1.UpdateStrategy{
-				Type:           defaultUpdateType,
-				MaxUnavailable: &intstr.IntOrString{Type: intstr.String, StrVal: defaultMaxUnavailable},
-				MaxSurge:       &intstr.IntOrString{Type: intstr.String, StrVal: defaultMaxSurge},
-			},
-			GlobalConfig: virtoolv1alpha1.GlobalConfig{
-				Registry:        defaultRegistry,
-				ImagePullSecret: defaultImagePullSecret,
 			},
 		},
 		Status: virtoolv1alpha1.VirtoolAppStatus{
-			ComponentStatus: map[string]virtoolv1alpha1.ComponentStatus{},
-			Conditions:      []metav1.Condition{},
+			ComponentsStatus: []virtoolv1alpha1.ComponentStatus{},
+			Conditions:       []metav1.Condition{},
 		},
 	}
 
